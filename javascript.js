6954ef167234d849083bffb1892ab488a0fc7e55
Maylo0314 <maylo.elfrink@student.graafschapcollege.nl>
2026-09-08 21:48:26 +0200

    Links Added in About Me and a few bugs are fixed

--- javascript.js
+++ javascript.js
@@ -1,14 +1,67 @@
-let lastScrollTop = 0;
-const header = document.getElementById('main-header');
+    let lastScrollTop = 0;
+    const header = document.getElementById('main-header');
 
-window.addEventListener('scroll', () => {
-const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-if (currentScroll > lastScrollTop) {
-    header.classList.add('hide');
-} else {
-    header.classList.remove('hide');
-}
+        if (currentScroll > lastScrollTop) {
+            header.classList.add('hide');
+        } else {
+            header.classList.remove('hide');
+        }
 
-lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
-});
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Safari
+    });
+
+    /* Swiper */
+
+    var swiper = new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    /* Modal */
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+    const close = document.querySelector(".close");
+
+    document.querySelectorAll(".image-popup").forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const image = this.querySelector("img");
+
+            modalImage.src = image.src;
+            modalImage.alt = image.alt;
+
+            modal.style.display = "flex";
+
+            document.body.style.overflow = "hidden";
+        });
+    });
+
+    close.addEventListener("click", function() {
+        modal.style.display = "none";
+
+        document.body.style.overflow = "";
+    });
+
+    modal.addEventListener("click", function(e) {
+        if (e.target === modal) {
+            modal.style.display = "none";
+
+            document.body.style.overflow = "";
+        }
+    });
